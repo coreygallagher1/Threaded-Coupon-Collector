0058d661c757d91375c6c56bef2431f9dbe80530
Sussbus2000 <jacobsussman5@gmail.com>
2022-05-03 10:39:55 -0500

    Small changes

--- src/IndependentCollector.java
+++ src/IndependentCollector.java
@@ -2,8 +2,8 @@ package src;
 
 public class IndependentCollector extends Collector implements Runnable {
 
-	public IndependentCollector() {
-		
+	public IndependentCollector(int trials) {
+		super(trials);
 	}
 
 	public static void main(String[] args) {
--- src/SynchronizedCollector.java
+++ src/SynchronizedCollector.java
@@ -1,20 +1,26 @@
 package src;
 
 public class SynchronizedCollector extends Collector implements Runnable {
-
-	public SynchronizedCollector() {
-
+	static long startTime;
+	public SynchronizedCollector(int trials) {
+		super(trials);
+		//...
 	}
 
 	public static void main(String[] args) {
+		startTime = System.currentTimeMillis();
+		SynchronizedCollector test = new SynchronizedCollector(1000);
 		
+		test.run();
 
+		long duration = System.currentTimeMillis() - startTime;
+		System.out.println("Duration: " + duration);
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		//...
+		System.out.println("Tax me");
 	}
 
 }
